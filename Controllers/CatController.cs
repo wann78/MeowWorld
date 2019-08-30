682f4a3df9e3719281a2cfbe42c22d39f5229e37
wann78 <46367439+wann78@users.noreply.github.com>
2019-08-30 10:45:34 +0200

    Add files via upload

---
+++ Controllers/CatController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowWorld.Controllers
+{
+    public class CatController
+    {
+        [HttpGet("cat/{cat}")]
+        public string Cat(string cat)
+        {
+            switch (cat)
+            {
+                case "bill":
+                    return "Meow!";
+                case "steve":
+                    return "Purr...";
+            }
+
+            return string.Empty;
+        }
+
+        
+    }
+}
---
+++ Controllers/HordeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowWorld.Controllers
+{
+    public class HordeController : Controller
+    {
+        private static Dictionary<string, string> _Cats = new Dictionary<string, string>();
+
+        [HttpGet("horde/add/{cat}")]
+        public string Add(string cat, string sound)
+        {
+            _Cats[cat] = sound;
+
+            return String.Format("{0} added to the horde!", cat);
+        }
+
+        [HttpGet("horde/{cat}")]
+        public string Get(string cat)
+        {
+            if (!_Cats.ContainsKey(cat))
+            {
+                return "Cat not found.";
+            }
+
+            return _Cats[cat];
+        }
+    }
+}
